2af067e18a82e91ba89e1aae9af9fbab813d14a2
kwolell <kwolell@gmailc.om>
2024-07-11 11:28:20 +0900

    불필요 모듈 제거

--- resources/metadata.py
+++
@@ -1,9 +0,0 @@
-context_metadata = """
-CREATE TABLE IF NOT EXISTS employees (
-    employee_id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    hire_date TEXT,
-    salary REAL
-)
-"""
